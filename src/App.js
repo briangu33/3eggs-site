d763a42ef8069c1b81418b83c5703a6d2f2b6436
briangu33 <briangu33@gmail.com>
2019-09-11 11:20:52 -0700

    wip boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Box, Button, Collapsible, Heading, Grommet, Layer,
-  ResponsiveContext } from 'grommet';
-import { FormClose, Notification } from 'grommet-icons'
+import { Grommet } from 'grommet';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from './scenes/landing/Landing'
 
 const theme = {
   global: {
@@ -9,91 +9,23 @@ const theme = {
       brand: '#228B86',
     },
     font: {
-      family: 'Roboto',
+      family: 'Open Sans',
       size: '14px',
       height: '20px',
     },
   },
 };
 
-const AppBar = (props) => (
-  <Box
-    tag='header'
-    direction='row'
-    align='center'
-    justify='between'
-    background='brand'
-    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-    elevation='medium'
-    style={{ zIndex: '1' }}
-    {...props}
-  />
-);
-
 class App extends Component {
-  state = {
-    showSidebar: false,
-  }
-
   render() {
-    const { showSidebar } = this.state;
     return (
-      <Grommet theme={theme} full>
-        <ResponsiveContext.Consumer>
-          {size => (
-            <Box fill>
-              <AppBar>
-                <Heading level='3' margin='none'>My App</Heading>
-                <Button
-                  icon={<Notification/>}
-                  onClick={() => this.setState({ showSidebar: !showSidebar })}/>
-              </AppBar>
-              <Box direction='row' flex overflow={{horizontal: 'hidden'}}>
-                <Box flex align='center' justify='center'>
-                  app body
-                </Box>
-                {(!showSidebar || size !== 'small') ? (
-                  <Collapsible direction="horizontal" open={showSidebar}>
-                    <Box
-                      flex
-                      width='medium'
-                      background='light-2'
-                      elevation='small'
-                      align='center'
-                      justify='center'
-                    >
-                      sidebar
-                    </Box>
-                  </Collapsible>
-                ): (
-                  <Layer>
-                    <Box
-                      background='light-2'
-                      tag='header'
-                      justify='end'
-                      align='center'
-                      direction='row'
-                    >
-                      <Button
-                        icon={<FormClose />}
-                        onClick={() => this.setState({ showSidebar: false })}
-                      />
-                    </Box>
-                    <Box
-                      fill
-                      background='light-2'
-                      align='center'
-                      justify='center'
-                    >
-                      sidebar
-                    </Box>
-                  </Layer>
-                )}
-              </Box>
-            </Box>
-          )}
-        </ResponsiveContext.Consumer>
-      </Grommet>
+      <Router>
+        <Grommet theme={theme} full>
+          <Switch>
+            <Route exact path={'/'} component={Landing} />
+          </Switch>
+        </Grommet>
+      </Router>
     );
   }
 }
---
+++ src/components/header-bar/HeaderBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class HeaderBar extends Component {
+  render () {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default HeaderBar;
