b590884a52b50364f335579f97c7864b126c62ad
yangalexandery <yangalexandery@gmail.com>
2019-09-19 01:20:40 -0400

    made logo clickable to home page

--- src/components/header-bar/HeaderBar.js
+++ src/components/header-bar/HeaderBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import {
   Box,
   Button,
@@ -67,6 +68,8 @@ const bigHeaderBar = (
       <Image
         src={logoBlack}
         height='100%'
+        onClick={() => window.location='/'}
+        style={{cursor: 'pointer'}}
       />
     </Box>
     <Box
