a916b853d4338e638a23bc3ad1bf43f6bbd44976
yangalexandery <yangalexandery@gmail.com>
2019-09-19 22:31:01 -0400

    added headerbar to other pages

--- src/scenes/about/About.js
+++ src/scenes/about/About.js
@@ -7,6 +7,7 @@ import {
   ResponsiveContext,
 } from 'grommet/es6'
 import { FormClose, Notification } from 'grommet-icons/es6'
+import HeaderBar from '../../components/header-bar/HeaderBar'
 
 const AppBar = (props) => (
   <Box
@@ -33,54 +34,7 @@ class About extends Component {
       <ResponsiveContext.Consumer>
         {size => (
           <Box fill>
-            <AppBar>
-              <Heading level='3' margin='none'>My App</Heading>
-              <Button
-                icon={<Notification/>}
-                onClick={() => this.setState({ showSidebar: !showSidebar })}/>
-            </AppBar>
-            <Box direction='row' flex overflow={{horizontal: 'hidden'}}>
-              <Box flex align='center' justify='center'>
-                about body
-              </Box>
-              {(!showSidebar || size !== 'small') ? (
-                <Collapsible direction="horizontal" open={showSidebar}>
-                  <Box
-                    flex
-                    width='medium'
-                    background='light-2'
-                    elevation='small'
-                    align='center'
-                    justify='center'
-                  >
-                    sidebar
-                  </Box>
-                </Collapsible>
-              ): (
-                <Layer>
-                  <Box
-                    background='light-2'
-                    tag='header'
-                    justify='end'
-                    align='center'
-                    direction='row'
-                  >
-                    <Button
-                      icon={<FormClose />}
-                      onClick={() => this.setState({ showSidebar: false })}
-                    />
-                  </Box>
-                  <Box
-                    fill
-                    background='light-2'
-                    align='center'
-                    justify='center'
-                  >
-                    sidebar
-                  </Box>
-                </Layer>
-              )}
-            </Box>
+            <HeaderBar/>
           </Box>
         )}
       </ResponsiveContext.Consumer>
