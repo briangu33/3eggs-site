fa0b9d3cf0e6c329ea90373f1c8bc47329589184
briangu33 <briangu33@gmail.com>
2019-10-04 12:23:11 -0700

    random cleanup

--- src/scenes/store/Store.js
+++ src/scenes/store/Store.js
@@ -1,43 +1,15 @@
 import React, { Component } from 'react'
 import {
   Box,
-  Button,
-  Collapsible,
-  Heading, Layer,
-  ResponsiveContext,
 } from 'grommet/es6'
-import { FormClose, Notification } from 'grommet-icons/es6'
 import HeaderBar from '../../components/header-bar/HeaderBar'
 
-const AppBar = (props) => (
-  <Box
-    tag='header'
-    direction='row'
-    align='center'
-    justify='between'
-    background='brand'
-    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
-    elevation='medium'
-    style={{ zIndex: '1' }}
-    {...props}
-  />
-);
-
 class Store extends Component {
-  state = {
-    showSidebar: false,
-  }
-
   render() {
-    const { showSidebar } = this.state;
     return (
-      <ResponsiveContext.Consumer>
-        {size => (
-          <Box fill>
-            <HeaderBar/>
-          </Box>
-        )}
-      </ResponsiveContext.Consumer>
+      <Box>
+        <HeaderBar/>
+      </Box>
     );
   }
 }
